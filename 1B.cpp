761160b53d5d7fa133f6a0c76dc2f851144e7a1d
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-18 09:22:19 +0100

    .

--- 1B.cpp
+++ 1B.cpp
@@ -5,4 +5,8 @@
 int main() {
     srand(time(0));
 
-  
+    printf("Números aleatorios generados con semilla original:\n");
+    for (int i = 0; i < 5; ++i) {
+        int random_number = rand();
+        printf("%d\n", random_number);
+    }
