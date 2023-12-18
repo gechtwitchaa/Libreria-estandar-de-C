f023b89afe6f058a284c6f3e30c6325180391912
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-18 09:46:16 +0100

    .

--- 2B.cpp
+++ 2B.cpp
@@ -6,4 +6,10 @@
 int main() {
     char str[MAX_LENGTH];
 
+    printf("Ingresa una cadena: ");
+    if (fgets(str, sizeof(str), stdin) == NULL) {
+        printf("Error al leer la cadena.\n");
+        return 1;
+    }
+
  
