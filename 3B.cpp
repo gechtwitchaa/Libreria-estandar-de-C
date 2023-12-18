5d3381a611f122a867253dc2a1a40a10224c3f2f
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-18 09:54:21 +0100

    .

--- 3B.cpp
+++ 3B.cpp
@@ -15,4 +15,24 @@ int main() {
     fclose(file);
 
 
+    file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
+
+    char buffer[MAX_BUFFER_SIZE];
+
+
+    if (fscanf(file, "%99[^\n]", buffer) != 1) {
+        printf("Error al leer del archivo.\n");
+        return 1;
+    }
+
+    printf("Leído del archivo: %s\n", buffer);
+
     
+    fclose(file);
+
+    return 0;
+}
