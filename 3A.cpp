ae12028195ef4bbccc33e7958cfc9d5820d6a96e
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-18 09:49:49 +0100

    .

--- 3A.cpp
+++ 3A.cpp
@@ -3,4 +3,11 @@
 #define MAX_FILENAME_LENGTH 100
 
 int main() {
-  
+    char filename[MAX_FILENAME_LENGTH];
+    printf("Ingresa el nombre del archivo: ");
+    if (scanf("%99s", filename) != 1) {
+        printf("Error al leer el nombre del archivo.\n");
+        return 1;
+    }
+
+ 
