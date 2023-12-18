f11d808eef6c73af26f8b1be44e7f33c44f86132
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-18 09:17:47 +0100

    .

---
+++ 2a.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int numero1 = 10;
+    int numero2 = -5;
+
+    int absoluto1 = abs(numero1);
+    int absoluto2 = abs(numero2);
+
+    printf("El valor absoluto de %d es: %d\n", numero1, absoluto1);
+    printf("El valor absoluto de %d es: %d\n", numero2, absoluto2);
+
+    return 0;
+}
