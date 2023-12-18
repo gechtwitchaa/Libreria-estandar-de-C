77436359f9c183c1c850294066652aca1c1ac237
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-18 09:46:33 +0100

    .

--- 2B.cpp
+++ 2B.cpp
@@ -12,4 +12,14 @@ int main() {
         return 1;
     }
 
- 
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+
+    int length = strlen(str);
+
+    printf("La longitud de '%s' es: %d\n", str, length);
+
+    return 0;
+}
