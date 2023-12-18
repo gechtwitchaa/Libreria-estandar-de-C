d35942cfa2fa83172b1040cf8b91057f1f2a74bb
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-18 09:53:49 +0100

    .

--- 3B.cpp
+++ 3B.cpp
@@ -2,3 +2,11 @@
 
 #define MAX_BUFFER_SIZE 100
 
+int main() {
+    FILE *file = fopen("file.txt", "w");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para escritura.\n");
+        return 1;
+    }
+
+  
